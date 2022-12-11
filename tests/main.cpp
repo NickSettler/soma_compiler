0045d6c92ef96bcab1402f5d0d321046c0c4e509
NickSettler <moiseevnikita14@gmail.com>
2022-12-11 18:45:01 +0100

    feat: initial commit

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+/**
+ *
+ * @file: main.cpp
+ * @date: 11.12.2022
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
