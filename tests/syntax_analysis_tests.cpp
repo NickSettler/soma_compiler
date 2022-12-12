1a48dc3a4a7e3afc11244d0e00f00d36a0a1f1a0
NickSettler <moiseevnikita14@gmail.com>
2022-12-12 23:18:06 +0100

    test(syntax analysis): adds semicolons to existing tests

--- tests/syntax_analysis_tests.cpp
+++ tests/syntax_analysis_tests.cpp
@@ -55,33 +55,33 @@ namespace soma {
             }
 
             TEST_F(SyntaxAnalysisTests, SingleNumbers) {
-                CheckSyntaxTree("1", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL});
+                CheckSyntaxTree("1;", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1.2e-1", {SYN_NODE_SEQUENCE, SYN_NODE_FLOAT_LITERAL});
+                CheckSyntaxTree("1.2e-1;", {SYN_NODE_SEQUENCE, SYN_NODE_FLOAT_LITERAL});
             }
 
             TEST_F(SyntaxAnalysisTests, ArithmeticExpressions) {
-                CheckSyntaxTree("1 + 2",
+                CheckSyntaxTree("1 + 2;",
                                 {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1 + 2e-2",
+                CheckSyntaxTree("1 + 2e-2;",
                                 {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_FLOAT_LITERAL});
 
-                CheckSyntaxTree("1 * 2 + 3", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL,
-                                              SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL});
+                CheckSyntaxTree("1 * 2 + 3;", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL,
+                                               SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1 * (2 + 3)", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL,
-                                                SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL});
+                CheckSyntaxTree("1 * (2 + 3);", {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL,
+                                                 SYN_NODE_INTEGER_LITERAL, SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1 * 2 + 3 / 4",
+                CheckSyntaxTree("1 * 2 + 3 / 4;",
                                 {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL, SYN_NODE_INTEGER_LITERAL,
                                  SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL, SYN_NODE_DIV, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1 * (2 + 3) / 4",
+                CheckSyntaxTree("1 * (2 + 3) / 4;",
                                 {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL, SYN_NODE_INTEGER_LITERAL,
                                  SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL, SYN_NODE_DIV, SYN_NODE_INTEGER_LITERAL});
 
-                CheckSyntaxTree("1 * (2 + (3 / 4) * 5)",
+                CheckSyntaxTree("1 * (2 + (3 / 4) * 5);",
                                 {SYN_NODE_SEQUENCE, SYN_NODE_INTEGER_LITERAL, SYN_NODE_MUL, SYN_NODE_INTEGER_LITERAL,
                                  SYN_NODE_ADD, SYN_NODE_INTEGER_LITERAL, SYN_NODE_DIV, SYN_NODE_INTEGER_LITERAL,
                                  SYN_NODE_MUL, SYN_NODE_INTEGER_LITERAL});
